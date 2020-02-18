32f85f50ef4b1637602a256263596713f02d9bdb
Arthur Andrade <arthur.diegoo@hotmail.com>
2020-02-18 16:15:38 -0300

    Add Bull Board
    The funcionality from the Bull Package shows in real time all the jobs scheduled all delays and let you also retry it from it.
    
    It's a great Admin Dashboard, but on production needs to be backed up by an Authentication route.

--- src/server.js
+++ src/server.js
@@ -3,12 +3,19 @@ import "dotenv/config";
 // Setup the Server
 import express from "express";
 import SubscriptionController from "./app/controllers/SubscriptionController";
-const app = express();
+import BullBoard from "bull-board";
+import Queue from "./app/tasks/Queue";
 
+const app = express();
+// Adding BullBoard for visualization of the Queues
+BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
+// Set express to use JSON
 app.use(express.json());
 // As this is a boilerplate app, i'll be using the Route inside the server file, but feel free to import your router
 app.post("/subscribe", SubscriptionController.store);
-
+// Creating the Admin route
+// !Remember to add Authentication for this route
+app.use("/admin/jobs", BullBoard.UI); 
 // Listen to port (Change for your Environment Variables if needed)
 app.listen(5050, () => {
   console.log("Server Running - Listening to Port: 5050");
